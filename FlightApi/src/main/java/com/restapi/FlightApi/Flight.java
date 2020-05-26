e17597054fa328fa91bbc5d7576ce4ab634a032e
Yashraj Balidani <ybalidani19.yr@gmail.com>
2020-05-26 20:30:19 +0530

    Flight Api Initialised

---
+++ FlightApi/src/main/java/com/restapi/FlightApi/Flight.java
@@ -0,0 +1,66 @@
+package com.restapi.FlightApi;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement           //this converts class member to Json Or Xml binding type
+public class Flight 
+{
+	private int id;                 //flight id
+	private String DeptApt;         //Departure Airport
+	private String ArrApt;          //Arrival Airport
+	private String DeptTime;        //Departure Time
+	private String ArrTime;         //Arrival Time
+	private int Distance;           //Distance Between DeptApt and ArrApt
+	private int Price;              //Price according to distance
+	private String type;            //Type : Domestic(D) or  International (I)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDeptApt() {
+		return DeptApt;
+	}
+	public void setDeptApt(String deptApt) {
+		DeptApt = deptApt;
+	}
+	public String getArrApt() {
+		return ArrApt;
+	}
+	public void setArrApt(String arrApt) {
+		ArrApt = arrApt;
+	}
+	public String getDeptTime() {
+		return DeptTime;
+	}
+	public void setDeptTime(String deptTime) {
+		DeptTime = deptTime;
+	}
+	public String getArrTime() {
+		return ArrTime;
+	}
+	public void setArrTime(String arrTime) {
+		ArrTime = arrTime;
+	}
+	public int getDistance() {
+		return Distance;
+	}
+	public void setDistance(int distance) {
+		Distance = distance;
+	}
+	public int getPrice() {
+		return Price;
+	}
+	public void setPrice(int price) {
+		Price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
